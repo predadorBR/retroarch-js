59a45cb66c9c7d34ee2043e9dd26c584ce09a026
predadorBR <151166110+predadorBR@users.noreply.github.com>
2024-02-13 19:16:39 -0300

    Add files via upload

---
+++ retroarch-core/src/index.js
@@ -0,0 +1,4 @@
+export { Retroarch } from "./Retroarch.js"
+export { buildCore } from "./buildCore.js"
+export { RetroarchConfig } from "./config.js"
+export { createRetroarch, fetchCore, CoreOptions } from "./createRetroarch.js"
